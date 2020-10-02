1d3b3ce9c0df9a5442781eed68be2b513c2f2d4b
1337Roly2 <72275578+1337Roly2@users.noreply.github.com>
2020-10-02 22:17:13 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import discord
+ 
+from discord.ext import commands
+from text_bot import TextBot
+ 
+bot = commands.Bot(
+	command_prefix=commands.when_mentioned_or('!'),
+	description='BOT'
+)
+ 
+bot.add_cog(TextBot(bot))
+ 
+@bot.event
+async def on_ready():
+	print('Logged in as:\n{0} (ID: {0.id})'.format(bot.user))
+	
+	await bot.change_presence(status = discord.Status.online, activity = discord.Game('Чёрную Метку'))
+ 
+bot.run('NjU5NzQ2MjkyNjgzMTEyNDU4.XgSynQ.F7zmQnNuJfmTlIIMLRHO87N8MqQ')
---
+++ text_bot.py
@@ -0,0 +1,32 @@
+from discord.ext import commands
+from discord.utils import get
+ 
+words = ['чм','черн','метка','black','mark','gang','чёрная','черный','чёрный','стиль','стил','legen','легенд']  
+ 
+class TextBot(commands.Cog):
+ 
+    def __init__(self, bot):
+        self.bot = bot
+ 
+    @commands.Cog.listener()
+    async def on_message(self, ctx):
+        """ Emoji reacts to someone who says"""
+        if ctx.author == self.bot.user:
+            return
+ 
+        if (any(st in ctx.content.lower() for st in words)):
+ 
+            await ctx.add_reaction(get(self.bot.emojis, name='c1')) 
+            await ctx.add_reaction(get(self.bot.emojis, name='e1')) 
+            await ctx.add_reaction(get(self.bot.emojis, name='r1')) 
+            await ctx.add_reaction(get(self.bot.emojis, name='n1')) 
+            await ctx.add_reaction(get(self.bot.emojis, name='a1')) 
+            await ctx.add_reaction(get(self.bot.emojis, name='ya')) 
+            await ctx.add_reaction(get(self.bot.emojis, name='bmg')) 
+            await ctx.add_reaction(get(self.bot.emojis, name='m1')) 
+            await ctx.add_reaction(get(self.bot.emojis, name='e2')) 
+            await ctx.add_reaction(get(self.bot.emojis, name='t1')) 
+            await ctx.add_reaction(get(self.bot.emojis, name='k1')) 
+            await ctx.add_reaction(get(self.bot.emojis, name='a2')) 
+            #await ctx.add_reaction('👀')
+            await self.bot.process_commands(ctx)
