2dd26f0be91bd0a343aa98d187731bd19d5cdd3e
1337Roly2 <72275578+1337Roly2@users.noreply.github.com>
2020-10-03 13:33:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ bot.add_cog(TextBot(bot))
 async def on_ready():
 	print('Logged in as:\n{0} (ID: {0.id})'.format(bot.user))
 	
-	await bot.change_presence(status = discord.Status.online, activity = discord.Game('Чёрную Метку'))
+	#await bot.change_presence(status = discord.Status.online, activity = discord.Game('Чёрную Метку'))
+	await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="стрим Санчиза"))
  
 bot.run('NjU5NzQ2MjkyNjgzMTEyNDU4.XgSynQ.F7zmQnNuJfmTlIIMLRHO87N8MqQ')
