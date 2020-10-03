a54a4a288913341f319da0a5f4c1ce3c138422e5
1337Roly2 <72275578+1337Roly2@users.noreply.github.com>
2020-10-03 13:04:42 +0300

    Update text_bot.py

--- text_bot.py
+++ text_bot.py
@@ -34,32 +34,3 @@ class TextBot(commands.Cog):
             #await ctx.add_reaction('👀')
             await self.bot.process_commands(ctx)
             
-w2 = ['style','стил']
-
- 
-class TextBot(commands.Cog):
- 
-    def __init__(self, bot):
-        self.bot = bot
- 
-    @commands.Cog.listener()
-    async def on_message(self, ctx):
-        """ Emoji reacts to someone who says"""
-        if ctx.author == self.bot.user:
-            return
-       
-        if (any(st in ctx.content.lower() for st in w2)):
- 
-            await ctx.add_reaction(get(self.bot.emojis, name='style')) 
-            await ctx.add_reaction(get(self.bot.emojis, name='c5')) 
-            await ctx.add_reaction(get(self.bot.emojis, name='t5')) 
-            await ctx.add_reaction(get(self.bot.emojis, name='i5')) 
-            await ctx.add_reaction(get(self.bot.emojis, name='l5')) 
-            await ctx.add_reaction(get(self.bot.emojis, name='ya5')) 
-            await ctx.add_reaction(get(self.bot.emojis, name='probel')) 
-            await ctx.add_reaction(get(self.bot.emojis, name='n5')) 
-            await ctx.add_reaction(get(self.bot.emojis, name='e5')) 
-            await ctx.add_reaction(get(self.bot.emojis, name='t6')) 
-            await ctx.add_reaction(get(self.bot.emojis, name='style2')) 
-            #await ctx.add_reaction('👀')
-            await self.bot.process_commands(ctx)
