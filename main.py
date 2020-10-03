852b9e8b819b0496ee340ebd3a20fc3211323fd7
1337Roly2 <72275578+1337Roly2@users.noreply.github.com>
2020-10-03 13:15:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,6 @@ bot.add_cog(TextBot(bot))
 async def on_ready():
 	print('Logged in as:\n{0} (ID: {0.id})'.format(bot.user))
 	
-	await bot.change_presence(status = discord.Status.online, activity = discord.status('Чёрную Метку'))
+	await bot.change_presence(status = discord.Status.online, activity = discord.game('Чёрную Метку'))
  
 bot.run('NjU5NzQ2MjkyNjgzMTEyNDU4.XgSynQ.F7zmQnNuJfmTlIIMLRHO87N8MqQ')
