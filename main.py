edcfa40597d6952c98b5cf5e686836cb005d0a5a
1337Roly2 <72275578+1337Roly2@users.noreply.github.com>
2020-10-05 00:30:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,36 +4,36 @@ from discord.ext import commands, tasks
 from text_bot import TextBot
 from itertools import cycle
  
-bot = commands.Bot(command_prefix=commands.when_mentioned_or('!'))
+client = commands.Bot(command_prefix=commands.when_mentioned_or('!'))
 
 
 status = cycle(['стрим Санчиза','аккаунты на FunPay','стрим Пахана','стрим Стила','канал #about-us','стрим Эскобарова','за блоком']) 
-bot.add_cog(TextBot(bot))
+client.add_cog(TextBot(client))
  
-@bot.event
+@client.event
 async def on_ready():
 	change_status.start()
-	print('Logged in as:\n{0} (ID: {0.id})'.format(bot.user))
+	print('Logged in as:\n{0} (ID: {0.id})'.format(client.user))
 
 
-@bot.command(pass_context=True)
+@client.command(pass_context=True)
 async def join(ctx):
 	channel = ctx.message.author.voice.voice_channel
-	awiat bot.join_voice_channel(channel)
+	awiat client.join_voice_channel(channel)
 
 
 @tasks.loop(seconds=60)
 async def change_status():
-	await bot.change_presence(status = discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name=(next(status))))
+	await client.change_presence(status = discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name=(next(status))))
 
 
 
-@bot.event
+@client.event
 async def on_member_join(member):
 	print(f'{member} joined.')
 
-@bot.event
+@client.event
 async def on_member_remove(member):
 	print(f'{member} left.')
 
-bot.run('NjU5NzQ2MjkyNjgzMTEyNDU4.XgSynQ.F7zmQnNuJfmTlIIMLRHO87N8MqQ')
+client.run('NjU5NzQ2MjkyNjgzMTEyNDU4.XgSynQ.F7zmQnNuJfmTlIIMLRHO87N8MqQ')
