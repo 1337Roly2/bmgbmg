3fc5067aca04113bc6a3a710bc21e76cc730f926
1337Roly2 <72275578+1337Roly2@users.noreply.github.com>
2020-10-05 00:02:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,18 @@ async def on_ready():
 @tasks.loop(seconds=60)
 async def change_status():
 	await bot.change_presence(status = discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name=(next(status))))
-	
+
+@bot.command(pass_context-True)
+async def join(ctx):
+	channel - ctx.message.author.voice.voice_channel
+	awiat bot.join_voice_channel(channel)
+
+@bot.command(pass_context-True)
+async def leave(ctx):
+	server = ctx.message.server
+	voice_bot = bot.voice_bot_in(server)
+	awiat voice_bot.disconnetc()
+
 @bot.event
 async def on_member_join(member):
 	print(f'{member} joined.')
