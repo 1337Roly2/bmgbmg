7a05c1d286a250c02238cf797e1f9e04fefd2ed7
1337Roly2 <72275578+1337Roly2@users.noreply.github.com>
2020-10-04 23:11:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,5 +16,15 @@ async def on_ready():
 	
 	#await bot.change_presence(status = discord.Status.online, activity = discord.Game('Чёрную Метку'))
 	await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="стрим Санчиза"))
- 
+
+@bot.event
+async def on_member_join(member):
+	print(f'{member} joined.')
+
+@bot.event
+async def on_member_remove(member):
+	print(f'{member} left.')
+	      
+	
+	
 bot.run('NjU5NzQ2MjkyNjgzMTEyNDU4.XgSynQ.F7zmQnNuJfmTlIIMLRHO87N8MqQ')
