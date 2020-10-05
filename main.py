706e52b892f27843c4c42d0b4924b1c666e2f2d9
1337Roly2 <72275578+1337Roly2@users.noreply.github.com>
2020-10-05 11:51:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,26 +19,4 @@ async def on_ready():
 async def change_status():
 	await client.change_presence(status = discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name=(next(status))))
 
-@client.event
-async def on_member_join(member):
-	print(f'{member} joined.')
-
-@client.event
-async def on_member_remove(member):
-	print(f'{member} left.')
-     
-@client.command()
-async def emb(ctx):
-	channel = ctx.message.channel
-	embed = discord.Embed(
-		title='Title',
-		description='Lorem Ipsum asdasd',
-		colour=discord.Colour.black()
-	)
-	
-	embed.set_author(name='Marco')
-	embed.add_field(name='Fueld name', value='Field Name', inline=False)
-	# embed.set_thumbnail(url="https://pluralsight.imgix.net/paths/python-7be70baaac.png")
-	await ctx.say(embed=embed)
-
 client.run('NjU5NzQ2MjkyNjgzMTEyNDU4.XgSynQ.F7zmQnNuJfmTlIIMLRHO87N8MqQ')
