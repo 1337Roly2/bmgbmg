3234a578430ae12b5b86adfad7d04cfc64916722
1337Roly2 <72275578+1337Roly2@users.noreply.github.com>
2020-10-05 09:56:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,22 +2,43 @@ import discord
  
 from discord.ext import commands, tasks
 from text_bot import TextBot
+from itertools import cycle
  
-client = commands.Bot(command_prefix = '.')
-
-
-
-client.add_cog(TextBot(client))
+bot = commands.Bot(
+	command_prefix=commands.when_mentioned_or('!'),
+	description='BOT'
+)
+status = cycle(['стрим Санчиза','аккаунты на FunPay','стрим Пахана','стрим Стила','канал #about-us','стрим Эскобарова','за блоком']) 
+bot.add_cog(TextBot(bot))
  
-@client.event
+@bot.event
 async def on_ready():
-	print('Logged in as:\n{0} (ID: {0.id})'.format(client.user))
+	change_status.start()
+	print('Logged in as:\n{0} (ID: {0.id})'.format(bot.user))
+	       
+@tasks.loop(seconds=60)
+async def change_status():
+	await bot.change_presence(status = discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name=(next(status))))
 
 @client.command(pass_context=True)
 async def join(ctx):
-    author = ctx.message.author
-    channel = author.voice_channel
-    await client.join_voice_channel(channel)
+	channel = ctx.message.author.voice.voice_channel
+	awiat client.join_voice_channel(channel)
 
+@bot.command(pass_context=True)
+async def leave(ctx):
+	server = ctx.message.server
+	voice_bot = bot.voice_bot_in(server)
+	awiat voice_bot.disconnect()
 
-client.run('NjU5NzQ2MjkyNjgzMTEyNDU4.XgSynQ.F7zmQnNuJfmTlIIMLRHO87N8MqQ')
+@bot.event
+async def on_member_join(member):
+	print(f'{member} joined.')
+
+@bot.event
+async def on_member_remove(member):
+	print(f'{member} left.')
+	      
+	
+	
+bot.run('NjU5NzQ2MjkyNjgzMTEyNDU4.XgSynQ.F7zmQnNuJfmTlIIMLRHO87N8MqQ')
